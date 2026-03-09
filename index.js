3e6424a262c0f42b77baae5fa1b626d703098263
Bernhard Pottler <bpottler@nexgo.de>
2026-03-09 06:05:45 +0100

    chore(deps): update esday from version 0.5.0 to 0.6.3

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ esday.extend(advancedFormatPlugin);
 const now = esday();
 const in5Days = now.add(5, 'days');
 
-console.log('Demo for ESday v0.5.0');
+console.log('Demo for ESday v0.6.3');
 console.log(`Now it is ${now.format('YYYY-MM-DD HH:mm:ss')}`);
 console.log(`In 5 days it will be ${in5Days.format('YYYY-MM-DD HH:mm:ss')}`);
